3088aa73461bf81c5ac5306f83d33743541ad0c3
ido maron <ido@sola.co.il>
2013-11-16 00:47:26 +0200

    Friday 15 Nov

--- server.js
+++ server.js
@@ -32,19 +32,22 @@ var campaign = sq.define('campaign',{name:Sq.TEXT, description:Sq.TEXT, hour:Sq.
 var node = sq.define('node',{nodeMac:Sq.TEXT});
 var filter = sq.define('filter',{name:Sq.TEXT});
 var activation = sq.define('activation',{mPhone:Sq.TEXT, deviceMac:Sq.TEXT, nodeMac:Sq.TEXT});
-var reg = sq.define('registration',{mPhone:Sq.TEXT, deviceMac:Sq.TEXT, roostDeviceToken:Sq.TEXT});
+var reg = sq.define('registration',{mPhone:Sq.TEXT, deviceMac:Sq.TEXT, roostDeviceToken:Sq.TEXT, isRoostActive:Sq.BOOLEAN});
+var tM = sq.define('trigerredMessage',{type:Sq.TEXT, text:Sq.TEXT, link:Sq.TEXT, isActive:Sq.BOOLEAN});
 
 manager
 	.hasMany(campaign)
 	.hasMany(node)
 	.hasMany(filter)
 	.hasMany(activation)
-	.hasMany(reg);
+	.hasMany(reg)
+    .hasMany(tM);
 campaign.belongsTo(manager);
 node.belongsTo(manager);
 filter.belongsTo(manager);
 activation.belongsTo(manager);
 reg.belongsTo(manager);
+tM.belongsTo(manager);
 
 sq.sync();
 
@@ -55,8 +58,8 @@ function sendError(res,message){
 	res.send('Error: '+ message);
 }
 
-
-//IVR Callbacks listners:
+//============= LISNERS ===========//
+//Listner to IVR, write to activation + update ivrId with ivrPhone
 app.get('/ivrcb',function(req,res){
 	console.log(req.query);
 	var rQ = req.query;
@@ -71,7 +74,8 @@ app.get('/ivrcb',function(req,res){
 	res.send(200, 'ok');
 });
 
-//Widget listners + allow cross domain
+//Listen to widget #1, Register+ create activation if by Wifi   //TODO- add 'register-by-device-mac'
+    //by phone
 app.all('/register-by-phone', function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     //res.header("Access-Control-Allow-Headers", "X-Requested-With");  //TODO make it specific here?
@@ -102,35 +106,75 @@ app.all('/register-by-phone', function(req, res) {
     	});
 	}else{}//no params, do nothing
 });
+    //by wifi
+app.all('/register-by-device-mac', function(req, res) {
+    console.log(req.body);
+    res.header("Access-Control-Allow-Origin", "*");
+    //res.header("Access-Control-Allow-Headers", "X-Requested-With");  //TODO make it specific here?
+    var rB = req.body;
+    if(rB.deviceMac && rB.nodeMac){
+        node.find({where:{}}).success(function(n){
+            if(n){
+                manager.find({where:{id:n.managerId}}).success(function(m){
+                    if(m){
+                        reg.find({where:{deviceMac:rB.deviceMac, managerId: m.id}}).success(function(r){
+                            if(r){console.log('already registered');}//do nothing
+                            else{
+                                reg.find({where:{mPhone:rB.mPhone, managerId: m.id}}).success(function(r){
+                                    if(r){
+                                        r.deviceMac = rB.deviceMac; 
+                                        r.save().success(function(){
+                                            console.log('added deviceMac to existing reg!');
+                                            res.send('added deviceMac to existing reg!');
+                                        });
+                                    }else{
+                                        reg.create({mPhone:rB.mPhone, managerId: m.id, deviceMac:rB.deviceMac}).success(function(){ //note: adding here all we got from widget #2
+                                            console.log('new reg created');
+                                            res.send('new reg created');
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }else{console.log('no manager found');}
+                });
+            }else{console.log('no node found');}
+        });
+    }else{console.log('Error: sent from widget without device & node params !');}//no params, do nothing
+});
+
+//Listen to widget #2, Send push notification on second roost download if the registeration isRoostActive: 
 app.all('/store-requested', function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     //res.header("Access-Control-Allow-Headers", "X-Requested-With");  //TODO make it specific here?
     var rB = req.body;
     console.log(rB);
-    if(rB.mPhone && rB.ivrId){
+    if(rB.mPhone && rB.ivrId){ //TODO unify with device MAc
     	manager.find({where:{ivrId:rB.ivrId}}).success(function(m){
 			if(m){
 				reg.find({where:{mPhone:rB.mPhone, managerId: m.id}}).success(function(r){
     				if(r){
-    					if(r.roostDeviceToken){
-    						//sendNotification
-    						console.log('sending notification for welcome back (mp)'); res.send('sending notification for welcome back (mp)');
+    					if(r.roostDeviceToken && r.isRoostActive){
+    						sendPushNot(r,'roost-reg-welcome-back',m);
+                            //setTimeout(function(){sendPushNot(r,'roost-reg-welcome-back',m); console.log('push note sent');},60000);
+    						console.log('sending notification for welcome back (mp)');
     					}else{console.log('new cutomer'); res.send('new cutomer');}//do nothing
 					}else{console.log('error: reg not found'); res.send('error: reg not found');}
 				});
 			}else{console.log('error: manager not found'); res.send('error: manager not found');}
 		});
-    }else if(rB.deviceMac && rB.nodeMac){console.log('device');
+    }else if(rB.deviceMac && rB.nodeMac){ //TODO unify with mPhone
+        console.log('device');
     	node.find({where:{nodeMac:rB.nodeMac}}).success(function(n){
     		if(n){
     			manager.find({where:{id:n.managerId}}).success(function(m){ //TODO add manager get by node function
 					if(m){
 						reg.find({where:{deviceMac:rB.deviceMac, managerId: m.id}}).success(function(r){
     						if(r){
-								if(r.roostDeviceToken){
-									//sendNotification
-									console.log('sending notification for welcome back'); res.send('sending notification for welcome back');
-								}else{console.log('new cutomer'); res.send('new cutomer');}//do nothing
+								if(r.roostDeviceToken && r.isRoostActive){
+									setTimeout(function(){sendPushNot(r,'roost-reg-welcome-back',m); console.log('push note sent');},60000);
+									console.log('sending notification for welcome back (wifi)');
+								}else{console.log('ROOSt will send notification');}//do nothing
 							}else{sendError(res, 'reg not found');}
 						});
 					}else{sendError(res, 'manager not found');}
@@ -140,75 +184,166 @@ app.all('/store-requested', function(req, res) {
     }else{sendError(res, 'no cookies sent');}
 });
 
-//ROOST Callback Lisner & functions:
-app.post('/roost-cb',function(req,res){
+//Listen to ROOST. 1) On reg- Update deviceTokens. 2)On un-reg- send cancelation SMS //TODO - should write it shorter....
+app.post('/roost-cb',function(req,res){ //TODO I think I can write this one shorter...
     console.log(req.body);
+
     var rB = req.body;
-    if(rB.enabled==true){
-        roostReg(rB);
-    }else if(rB.enabled==false){
-        roostUnReg(rB);
-    }else{console.log('Error: user enabled id undefined');}
-}
-function roostReg(rB){
-    var mPhoneTag='';
-    //getting the mPhone //TODO getting also mdevice if no phone is here
+    var tagType='';
+    var tagValue='';
+    //getting the identifier (mPhone OR deviceMac)
     for(var t in rB.tags){
-        var tag = rB.tags[t].split("-");
-        if(tag[0]=='mp'){
-            mPhoneTag= tag[1];
-            console.log('phone is: '+tag[1]);
+        var tagParts = rB.tags[t].split("-");
+        if(tagParts[0]=='mp'){
+            tagType = 'mPhone';
+            tagValue= tagParts[1];
+            console.log('phone is: '+tagParts[1]);
             break;
-        }else if(tag[0]=='dm'){
-            deviceMacTag= tag[1];
-            console.log('device_mac is: '+tag[1]);
+        }else if(tagParts[0]=='dm'){
+            tagType = 'deviceMac';
+            tagValue= tagParts[1];
+            console.log('device_mac is: '+tagParts[1]);
             break;
         }
     }
-    if(mPhone){
+    if(tagType && tagValue){
         manager.find({where:{roostConfKey: rB.appKey}}).success(function(m){
-            if(m){
-                reg.find({where:{managerId:m.id, mPhone:mPhoneTag}}).success(function(r){
-                    if(r){
-                        r.roostDeviceToken = rB.device_token; //note: no need to check if it is returning customer- ROOST will send welcome anyways....
-                        r.save().success(function(){console.log('Device token added to registration');});
-                    }
-                });
+            if(m){        
+                if(rB.enabled==true){
+                    roostReg(rB , tagType, tagValue, m);
+                }else if(rB.enabled==false){
+                    roostUnReg(rB, tagType, tagValue, m);
+                }else{console.log('Error: user enabled is undefined');}
+            }else{console.log('Error: manager not found');}
+        });
+    }else{console.log('Error: user has no identifiers in roost');}
+});
+
+function roostReg(rB, tagType, tagValue, m){
+    if(tagType=='mPhone'){ //TODO add method of save device token to reg instance
+        reg.find({where:{managerId:m.id, mPhone:tagValue}}).success(function(r){
+            if(r){
+                r.roostDeviceToken = rB.device_token; //note: no need to check if it is returning customer- ROOST will send welcome anyways....
+                r.isRoostActive = true;
+                r.save().success(function(){console.log('Device token added to registration');});
             }
         });
-    }else if(mPhone){
-        manager.find({where:{roostConfKey: rB.appKey}}).success(function(m){
-            if(m){
-                reg.find({where:{managerId:m.id, mPhone:mPhoneTag}}).success(function(r){
-                    if(r){
-                        r.roostDeviceToken = rB.device_token; //note: no need to check if it is returning customer- ROOST will send welcome anyways....
-                        r.save().success(function(){console.log('Device token added to registration');});
-                    }
-                });
+    }else if(tagType=='deviceMac'){ //TODO can to unify this and mPhone
+        reg.find({where:{managerId:m.id, deviceMac:tagValue}}).success(function(r){
+            if(r){
+                r.roostDeviceToken = rB.device_token; //note: no need to check if it is returning customer- ROOST will send welcome anyways....
+                r.isRoostActive = true;
+                r.save().success(function(){console.log('Device token added to registration');});
+            }
+        });
+    }    
+}
+
+function roostUnReg(rB, tagType, tagValue, m){
+    if(tagType=='mPhone'){ //TODO add method of make roost not active to reg instance
+        reg.find({where:{managerId:m.id, mPhone:tagValue}}).success(function(r){
+            if(r){
+                r.isRoostActive = false; 
+                r.save().success(function(){console.log('user un registered from ROOST, trying to send SMS');});
+                sendSms(r,'roost-cancelation',m);
+            }
+        });
+    }else if(tagType=='deviceMac'){ //TODO add method of make roost not active to reg instance
+        reg.find({where:{managerId:m.id, deviceMac:mPhoneTag}}).success(function(r){
+            if(r){
+                r.isRoostActive = false; 
+                r.save().success(function(){console.log('Device token added to registration');});
+                sendSms(r,'roost-cancelation',m);
             }
         });
     }
 }
 
+//===== FUNCTIONS =====//
+//SEND SMS to a spcific reg
+function sendSms(reg, type, manager){
+    if(reg.managerId && reg.mPhone){
+        console.log(reg);
+        tM.find({where:{managerId:reg.managerId, type:type}}).success(function(tM){
+            if(tM){
+                var http = require('http');
+                var qs = require('querystring');
+                var smsData = qs.stringify({post:2,uid:'2561',un:'promonim',msg:tM.text,list:'05'+reg.mPhone,charset:'utf-8',from:'035555555'});
+                var options = {
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    host: 'www.micropay.co.il',
+                    method:'POST',
+                    path: '/ExtApi/ScheduleSms.php',
+                };
+                /*TODO the following came with the http function, need to organuize it in my code*/
+                callback = function(response) {
+                    var str = '';
+                    //another chunk of data has been recieved, so append it to `str`
+                    response.on('data', function (chunk) {
+                        str += chunk;
+                    });
+                    //the whole response has been recieved, so we just print it out here
+                    response.on('end', function () {
+                        console.log(str);
+                    });
+                }
+                /*TODO the following came with the http function----END---*/
+                var postReq = http.request(options, callback);
+                postReq.write(smsData);
+                postReq.end();
+            }else{concole.log('Error: no triggered message of this type fot this manager');}
+        });
+    }else{console.log('SMS not sent since reg has no mPhone (or no managerId)');}
+}
 
-//    var rBt = req.body.tags;
-	/*
-    if(rBt){
-		console.log(rBt);
-		for(var t in rBt){
-			console.log(rBt[t].toString().split('-')[0]);
-		}
-	}
-    */
-});
+//SEND PUSH Notification to a specific reg
+function sendPushNot(reg,type,manager){
+    if(reg.roostDeviceToken){
+        console.log(reg);
+        tM.find({where:{managerId:reg.managerId, type:type}}).success(function(tM){ //TODO maybe unify with function sendSms()
+            if(tM){
+                var https = require('https');
+                //var qs = require('querystring'); 
+                var data = JSON.stringify({alert:tM.text, url:tM.link,device_tokens:[reg.roostDeviceToken]}); //TODO add sound
+
+                var options = {
+                  hostname: 'launch.alertrocket.com',
+                  port: 443,
+                  path: '/api/push',
+                  method: 'POST',
+                  auth:manager.roostConfKey+':'+manager.roostSecretKey,
+                  //headers: { 'Content-Type': 'application/json','Content-Length': userString.length}
+                };
+
+                var req = https.request(options, function(res) {
+                  //console.log("statusCode: ", res.statusCode);
+                  //console.log("headers: ", res.headers);
+
+                  res.on('data', function(d) {
+                    //process.stdout.write(d); //TODO WHAT IS THIS??
+                  });
+                });
+                req.write(data);
+                req.end();
+
+                req.on('error', function(e) {
+                  console.error('error');
+                  console.error(e);
+                });
+            }else{concole.log('Error: no triggered message of this type fot this manager');}
+        });
+    }   
+}
 
 //optional: delete the unndecessary tags from moshe and me
-//finish the roost-cb function
+
 // download roost and send a tag mp-544585295
-//check that the widgets are working
 //connect to their message=ing system when on returning customer.
 //mimic ALL THIs to WIFI
 //complete scheduled campaigns
+
+
+
 
 
 
@@ -247,6 +382,8 @@ req.on('error', function(e) {
 /*EXAMPLE FOT CONTACTING ROOST API*/  /*WORKING---END*/
 
 /*EXAMPLE FOT CONTACTING ROOST API*/
+
+/*
 function sendPushNot(){
     var https = require('https');
     var qs = require('querystring'); 
@@ -278,3 +415,4 @@ function sendPushNot(){
       console.error(e);
     });
 }
+*/
